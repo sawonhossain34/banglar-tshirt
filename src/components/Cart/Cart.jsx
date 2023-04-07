f3c5b237b43b1ac2c1fce1faacdec0291b7dc454
sawonhossain34 <sawonhossain3491@gmail.com>
2023-04-07 12:39:22 +0600

    conditional randering if else && || class css conditional rendering add

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,21 +1,51 @@
 import React from 'react';
+import "./Cart.css"
 import { parsePath } from 'react-router-dom';
 
-const Cart = ({cart,handleRemovedFromCart}) => {
-//    console.log(cart)
+const Cart = ({ cart, handleRemovedFromCart }) => {
+
+    let message;
+    if (cart.length === 0) {
+        message = <p>Pleace add some products</p>
+    }
+    else {
+        message = <div>
+            <h3>Boroloxxx</h3>
+            <p><small>Thanks for giving</small></p>
+        </div>
+    }
     return (
         <div>
-            <h2>Order Summary : {cart.length}</h2>
+            <h2 className={cart.length > 2 ? "red" : 'blue'}>Order Summary : {cart.length}</h2>
+            
+            {cart.length > 2 ? <p>aro kino</p> : <p>fokira</p>}
+            {message}
             {
-                cart.map(tshirt => <p 
-                key={tshirt._id}>
-                    {tshirt.name }
+                cart.map(tshirt => <p
+                    key={tshirt._id}>
+                    {tshirt.name}
                     <button onClick={() => handleRemovedFromCart(tshirt._id)}>X</button>
-                    
-                    </p>)
+
+                </p>)
+            }
+            {
+                cart.length === 2 && <p>Duble bonanzara</p>
+            }
+            {
+                cart.length === 3 || <p>tinta to hoilo na!!!</p>
             }
         </div>
     );
 };
+
+//CONDITIONAL RANDERING
+// 1 if / else conditional randering
+// 2 ternary operator randaring
+// 3 logical condition && true 
+// 4 logical condition || false
+
+// CONDITIONAL CSS CLASS
+// 1 ternary css add 
+// 2 dynamic css add
 
 export default Cart;
